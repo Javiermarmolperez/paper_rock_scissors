5bac056fe2fb22fef84df8a3d43fa4eeb338704c
Javier <javiermarmolperez@gmail.com>
2021-12-06 12:37:18 +0100

    feat: first test in green

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,23 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:piedra_papel_tijeras/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      test('Counter value should be incremented', () {
+        final game = Game();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+        var anElection = "paper";
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+        var group = game.chooseElection(anElection);
+
+        expect(group, ["paper", ""]);
+      });
+}
+
+class Game {
+
+  chooseElection(String anElection) {
+    return [anElection,""];
+  }
 }
